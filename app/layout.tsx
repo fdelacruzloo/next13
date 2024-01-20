a479f7697ec27e0251fbc92f5e9950701e5990d3
fredydelacruz <148255743+fredydelacruz@users.noreply.github.com>
2024-01-20 12:04:29 -0500

    Segundi commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { SiteHeader } from "@/components/SiteHeader";
 import "./globals.css";
 import { AppProviders } from "@/components/AppProviders";
 
