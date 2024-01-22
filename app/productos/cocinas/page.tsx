6865a0026d5546c64e00f6496a263a8f5b0dc78d
fredydelacruz <148255743+fredydelacruz@users.noreply.github.com>
2024-01-21 19:47:08 -0500

    23 commit

--- app/productos/cocinas/page.tsx
+++ app/productos/cocinas/page.tsx
@@ -10,7 +10,6 @@ export default async function CocinasPage() {
 
       <div className="flex">
     
-        <div className="flex flex-wrap">
           {cocinas.map(({ id, title, imageUrl, link }) => (
             <div key={id} className="w-1/2 pr-2">
               <div className="border p-4 my-4 rounded-md">
@@ -20,7 +19,7 @@ export default async function CocinasPage() {
                     className="mb-2 rounded-md object-cover"
                   />                 
                 </a>   
-                <div className="text-xs" style={{ height: '80px' }}>
+                <div className="text-xs" style={{ height: '100px' }}>
                   <h1>{title}</h1>
                   <Counter />
                 </div>
