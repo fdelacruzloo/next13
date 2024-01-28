4db70a29a19080e993f6a13237cafccc9aa05175
fredydelacruz <148255743+fredydelacruz@users.noreply.github.com>
2024-01-28 09:28:28 -0500

    se ven todas las imagenes

--- components/CocinasPage.tsx
+++ components/CocinasPage.tsx
@@ -1,5 +1,5 @@
-"use client";
-import React, { useState, useEffect } from "react";
+"use client"
+import React, { useState, useEffect } from 'react';
 import { getCocinas } from "@/app/productos/list";
 import PhotoList from "@/components/Photos";
 
@@ -12,31 +12,22 @@ type Cocina = {
 
 const CocinasPage: React.FC = () => {
   const [cocinas, setCocinas] = useState<Cocina[]>([]);
-  const [isHighPressureClicked, setIsHighPressureClicked] = useState(false);
-  const [isLowPressureClicked, setIsLowPressureClicked] = useState(false);
 
   useEffect(() => {
     const fetchCocinas = async () => {
-      const result = await getCocinas(isHighPressureClicked, isLowPressureClicked);
+      const result = await getCocinas();
       setCocinas(result);
     };
 
     fetchCocinas();
-  }, [isHighPressureClicked, isLowPressureClicked]);
+  }, []);
 
   return (
     <div className="flex flex-col items-center">
-      <button onClick={() => setIsHighPressureClicked(!isHighPressureClicked)}>
-        Toggle High Pressure
-      </button>
-      <button onClick={() => setIsLowPressureClicked(!isLowPressureClicked)}>
-        Toggle Low Pressure
-      </button>
-      <div className="text-2xl py-5 flex items-center space-x-4"></div>
+      <div className="text-2xl py-5 flex items-center space-x-4">
+      </div>
       <div className="flex flex-wrap">
-        {cocinas.map((cocina) => (
-          <PhotoList key={cocina.id} {...cocina} />
-        ))}
+        {cocinas.map(cocina => <PhotoList key={cocina.id} {...cocina} />)}
       </div>
     </div>
   );
