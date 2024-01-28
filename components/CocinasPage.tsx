295dbc1b1a6f765e1fe87008940775578649479a
fredydelacruz <148255743+fredydelacruz@users.noreply.github.com>
2024-01-28 10:50:06 -0500

    todo ok antes de hacer cambios en list.ts

--- components/CocinasPage.tsx
+++ components/CocinasPage.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import { getCocinas } from "@/app/productos/list";
 import PhotoList from "@/components/Photos";
 
@@ -8,26 +8,41 @@ type Cocina = {
   title: string;
   imageUrl: string;
   cantidad: number;
+  regulador: boolean;
 };
 
 const CocinasPage: React.FC = () => {
   const [cocinas, setCocinas] = useState<Cocina[]>([]);
+  const [isHighPressureClicked, setIsHighPressureClicked] = useState(false);
+  const [isLowPressureClicked, setIsLowPressureClicked] = useState(false);
 
   useEffect(() => {
     const fetchCocinas = async () => {
-      const result = await getCocinas();
+      const result = await getCocinas(isHighPressureClicked, isLowPressureClicked);
       setCocinas(result);
     };
 
     fetchCocinas();
-  }, []);
+  }, [isHighPressureClicked, isLowPressureClicked]);
+
+  // Filtrar las cocinas basado en los estados de los botones
+  const filteredCocinas = cocinas.filter(cocina => {
+    if (isHighPressureClicked) {
+      return cocina.regulador === false;
+    }
+    if (isLowPressureClicked) {
+      return cocina.regulador === true;
+    }
+    return true;
+  });
 
   return (
     <div className="flex flex-col items-center">
-      <div className="text-2xl py-5 flex items-center space-x-4">
-      </div>
+      <div className="text-2xl py-5 flex items-center space-x-4"></div>
       <div className="flex flex-wrap">
-        {cocinas.map(cocina => <PhotoList key={cocina.id} {...cocina} />)}
+        {filteredCocinas.map((cocina) => (
+          <PhotoList key={cocina.id} {...cocina} />
+        ))}
       </div>
     </div>
   );
