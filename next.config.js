dff7a9f971d8ba88fbdede7e7189d6b94092f554
fredydelacruz <148255743+fredydelacruz@users.noreply.github.com>
2024-02-06 19:56:49 -0500

    con contadores corregido

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['pdf-viewe-2024.s3.amazonaws.com'], // Añade el dominio aquí
-    },
-  }
-  
-  module.exports = nextConfig;
+  images: {
+    domains: ['pdf-viewe-2024.s3.amazonaws.com'], // Añade el dominio aquí
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Configuración de caché para el lado del cliente
+      config.cache = {
+        type: 'filesystem',
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
